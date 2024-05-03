93bc8924bf884c2fa69b853c0ad17d399a5fea2e
Alia Ashraf <aliaashrafm@gmail.com>
2024-05-03 16:30:43 +0300

    added the styling and js of the recipe pic

--- index.js
+++ index.js
@@ -362,3 +362,26 @@ btn2.addEventListener("click", () => {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
       }
       
+/* ------------------------------------------------------------ */
+/* Chicken stir fry picture (to be edited)*/
+document.addEventListener('DOMContentLoaded', function() {
+  var myImage = document.getElementById('ChickenImages');
+  myImage.addEventListener('click', function() {
+      var currentSrc = myImage.getAttribute('src');
+      var alternateSrc = 'ChickStirFry1.jpeg';
+      if (currentSrc === 'ChickStirFry.jpg') {
+          myImage.style.opacity = 0;
+          setTimeout(function() {
+              myImage.setAttribute('src', alternateSrc);
+              myImage.style.opacity = 1;
+          }, 500);
+      } else {
+          myImage.style.opacity = 0;
+          setTimeout(function() {
+              myImage.setAttribute('src', 'ChickStirFry.jpg');
+              myImage.style.opacity = 1; 
+          }, 500); 
+      }
+  });
+});
+/* ------------------------------------------------------------ */
