8ca6bbb75f896fa3ab778bc311a5506ec9a7fbdf
mayarmm <mayarmro2003@gmail.com>
2024-05-04 22:57:01 +0300

    adding view ingrediants js

--- index.js
+++ index.js
@@ -776,5 +776,34 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
+/* --------------------------------------------------------------- */
+/* -------------------------view ingredinats----------------------- */
 
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li'); 
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate'); 
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => { 
+        option.addEventListener('click', (event) => { 
+            selected.innerText = event.target.innerText; 
+            options.forEach(option => { 
+                option.classList.remove('active');
+            });
+            event.target.classList.add('active');
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+        });
+    });
+});
 
