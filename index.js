80ec23a068be8b27d5f5c0dc374495cde64de687
mayarmm <mayarmro2003@gmail.com>
2024-05-06 12:48:53 +0300

    adding instgram page link , updating css

--- index.js
+++ index.js
@@ -787,33 +787,33 @@ document.addEventListener('DOMContentLoaded', () => {
 /* --------------------------------------------------------------- */
 /* -------------------------view ingredinats----------------------- */
 
-const dropdowns = document.querySelectorAll('dropdowningrediants');
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.ingrediantsmenu');
-    const options = dropdown.querySelectorAll('.ingrediantsmenu li'); 
-    const selected = dropdown.querySelector('.selected');
+// const dropdowns = document.querySelectorAll('dropdowningrediants');
+// dropdowns.forEach(dropdown => {
+//     const select = dropdown.querySelector('.select');
+//     const caret = dropdown.querySelector('.caret');
+//     const menu = dropdown.querySelector('.ingrediantsmenu');
+//     const options = dropdown.querySelectorAll('.ingrediantsmenu li'); 
+//     const selected = dropdown.querySelector('.selected');
 
-    select.addEventListener('click', () => {
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate'); 
-        menu.classList.toggle('ingrediantsmenu-open');
-    });
+//     select.addEventListener('click', () => {
+//         select.classList.toggle('select-clicked');
+//         caret.classList.toggle('caret-rotate'); 
+//         menu.classList.toggle('ingrediantsmenu-open');
+//     });
 
-    options.forEach(option => { 
-        option.addEventListener('click', (event) => { 
-            selected.innerText = event.target.innerText; 
-            options.forEach(option => { 
-                option.classList.remove('active');
-            });
-            event.target.classList.add('active');
-            select.classList.remove('select-clicked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('ingrediantsmenu-open');
-        });
-    });
-});
+//     options.forEach(option => { 
+//         option.addEventListener('click', (event) => { 
+//             selected.innerText = event.target.innerText; 
+//             options.forEach(option => { 
+//                 option.classList.remove('active');
+//             });
+//             event.target.classList.add('active');
+//             select.classList.remove('select-clicked');
+//             caret.classList.remove('caret-rotate');
+//             menu.classList.remove('ingrediantsmenu-open');
+//         });
+//     });
+// });
 
 // ........................................add js of adding users for admin...........................
 function togglePasswordVisibility(inputId) {
