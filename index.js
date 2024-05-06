8599ecbf64d5c783d8d250382148b5ae228544ed
mayarmm <mayarmro2003@gmail.com>
2024-05-06 16:31:07 +0300

    modifying view ingrediants css , html and js

--- index.js
+++ index.js
@@ -787,33 +787,62 @@ document.addEventListener('DOMContentLoaded', () => {
 /* --------------------------------------------------------------- */
 /* -------------------------view ingredinats----------------------- */
 
-const dropdowns1 = document.querySelectorAll('dropdowningrediants');
-dropdowns1.forEach(dropdowningrediants => {
-    const select1 = dropdowningrediants.querySelector('.selectt');
-    const caret1 = dropdowningrediants.querySelector('.carett');
-    const menu1 = dropdowningrediants.querySelector('.ingrediantsmenu');
-    const options1 = dropdowningrediants.querySelectorAll('.ingrediantsmenu li'); 
-    const selected1 = dropdowningrediants.querySelector('.selectedd');
+const dropdowns = document.querySelectorAll('.dropdowningrediants');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.selectt');
+    const caret = dropdown.querySelector('.carett');
+    const menu = dropdown.querySelector('.ingrediantsmenu');
+    const options = dropdown.querySelectorAll('.ingrediantsmenu li'); 
+    const selected = dropdown.querySelector('.selectedd');
 
-    select1.addEventListener('click', () => {
-        select1.classList.toggle('selectt-clicked');
-        caret1.classList.toggle('carett-rotate'); 
-        menu1.classList.toggle('ingrediantsmenu-open');
+    select.addEventListener('click', () => {
+        select.classList.toggle('selectt-clicked');
+        caret.classList.toggle('carett-rotate'); 
+        menu.classList.toggle('ingrediantsmenu-open');
     });
 
-    options1.forEach(option => { 
-        option1.addEventListener('click', (event) => { 
-            selected1.innerText = event.target.innerText; 
-            options1.forEach(option => { 
+    options.forEach(option => { 
+        option.addEventListener('click', (event) => { 
+            selected.innerText = event.target.innerText; 
+            options.forEach(option => { 
                 option.classList.remove('active');
             });
-            event1.target.classList.add('active');
-            select1.classList.remove('selectt-clicked');
-            caret1.classList.remove('carett-rotate');
-            menu1.classList.remove('ingrediantsmenu-open');
+            event.target.classList.add('active');
+            select.classList.remove('selectt-clicked');
+            caret.classList.remove('carett-rotate');
+            menu.classList.remove('ingrediantsmenu-open');
         });
     });
 });
+
+
+// const dropdowns1 = document.querySelectorAll('dropdowningrediants');
+// dropdowns1.forEach(dropdowningrediants => {
+//     const select1 = dropdowningrediants.querySelector('.selectt');
+//     const caret1 = dropdowningrediants.querySelector('.carett');
+//     const menu1 = dropdowningrediants.querySelector('.ingrediantsmenu');
+//     const options1 = dropdowningrediants.querySelectorAll('.ingrediantsmenu li'); 
+//     const selected1 = dropdowningrediants.querySelector('.selectedd');
+
+//     select1.addEventListener('click', () => {
+//         select1.classList.toggle('selectt-clicked');
+//         caret1.classList.toggle('carett-rotate'); 
+//         menu1.classList.toggle('ingrediantsmenu-open');
+//     });
+
+//     options1.forEach(option => { 
+//         option1.addEventListener('click', (event) => { 
+//             selected1.innerText = event.target.innerText; 
+//             options1.forEach(option => { 
+//                 option.classList.remove('active');
+//             });
+//             event1.target.classList.add('active');
+//             select1.classList.remove('selectt-clicked');
+//             caret1.classList.remove('carett-rotate');
+//             menu1.classList.remove('ingrediantsmenu-open');
+//         });
+//     });
+// });
 
 // ........................................add js of adding users for admin...........................
 function togglePasswordVisibility(inputId) {
